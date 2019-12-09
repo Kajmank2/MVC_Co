c94d1dd66708299a55d75d4ba7a35423abc49bb0
Kajmank2 <49954039+Kajmank2@users.noreply.github.com>
2019-12-10 00:03:51 +0100

    FIrstDay
    Route, BasicOperations

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random ()
+        {
+            var movie = new Movie() { Name = "Karol" };
+            // return Content("Hello World");
+            return View(movie);
+            //return HttpNotFound();
+            // return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" }); ;
+
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sorbBy)
+        {
+            if (pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sorbBy)) ;
+            {
+                sorbBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}& SortBy={1}", pageIndex, sorbBy));
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
