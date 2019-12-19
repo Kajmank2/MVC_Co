c616ebebcda8c5755400271351ee893b8868ebb2
Kajmank2 <49954039+Kajmank2@users.noreply.github.com>
2019-12-20 00:10:18 +0100

    EXC2

---
+++ WebKUR/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebKUR.Models;
+
+namespace WebKUR.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+    }
+}
---
+++ WebKUR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebKUR.Startup))]
+namespace WebKUR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
